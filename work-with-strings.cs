23d1732b8c90a0fe51faa1ab7c486a8722aa91b8
Prisca <priscamasereli@gmail.com>
2026-09-15 18:30:06 +0200

    Search and replace text in strings

--- work-with-strings.cs
+++ work-with-strings.cs
@@ -1,6 +1,6 @@
 string firstFriend= "Prisca";
 string secondFriend= "Jean";
-//Console.WriteLine($"My friends are {firstFriend} and {secondFriend} and {firstFriend} has {firstFriend.Length} characters.");
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend} and {firstFriend} has {firstFriend.Length} characters.");
 
 
 // remove whitespace from a string
@@ -15,3 +15,18 @@ Console.WriteLine($"[{trimmedGreeting}]");
 
 trimmedGreeting = greeting.Trim();
 Console.WriteLine($"[{trimmedGreeting}]");
+
+// replace a substring in a string
+string sayHello= "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+Console.WriteLine(sayHello.Contains("Greetings"));
+Console.WriteLine(string.Join(", ", sayHello.Split(" ")));
+
+
+string songLyrics = "You say goodbye, and I say hello";
+Console.WriteLine(songLyrics.EndsWith("goodbye"));
+Console.WriteLine(songLyrics.StartsWith("You"));
