8ec5b28f644cb2ad88d5cd22d13d701f14f7a516
Prisca <priscamasereli@gmail.com>
2026-09-15 17:53:18 +0200

    Concatinating strings

---
+++ work-with-strings.cs
@@ -0,0 +1,3 @@
+string firstFriend= "Prisca";
+string secondFriend= "Jean";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend} and {firstFriend} has {firstFriend.Length} characters.");
