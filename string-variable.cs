ed23407a8c031df59c445a8920b00e4d7d640776
Prisca <priscamasereli@gmail.com>
2026-09-15 11:07:14 +0200

    Declare and use variables

---
+++ string-variable.cs
@@ -0,0 +1,6 @@
+string aFriend= "Prisca";
+Console.WriteLine(aFriend);
+aFriend ="Jean";
+Console.WriteLine("Hello, " + aFriend + "!");
+// Using string interpolation
+Console.WriteLine($"Hello, {aFriend}!");
