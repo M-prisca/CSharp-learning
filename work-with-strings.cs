9648d94c48f2adeba969d993d0eca3d512357452
Prisca <priscamasereli@gmail.com>
2026-09-15 18:07:04 +0200

    Removing spaces in a string

--- work-with-strings.cs
+++ work-with-strings.cs
@@ -1,3 +1,17 @@
 string firstFriend= "Prisca";
 string secondFriend= "Jean";
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend} and {firstFriend} has {firstFriend.Length} characters.");
+//Console.WriteLine($"My friends are {firstFriend} and {secondFriend} and {firstFriend} has {firstFriend.Length} characters.");
+
+
+// remove whitespace from a string
+string greeting= "             Hello world!             ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
